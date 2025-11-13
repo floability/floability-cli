01cf2db4eb3fe014cc56759b86f732e47b559f22
Saiful Islam <saifulislamplabon@gmail.com>
2025-11-13 12:10:15 -0500

    Updated floability version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="floability",
-    version="0.1.4-dev",
+    version="0.2.0-dev",
     description="Run distributed Jupyter-based workflows with TaskVine",
     long_description=long_description,
     long_description_content_type="text/markdown",
