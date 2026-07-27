276e8c2c5ac383aa747441e53a87a9f79eb838b0
Saiful Islam <saifulislamplabon@gmail.com>
2026-07-26 21:55:31 -0400

    Prepare release 0.2.1

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="floability",
-    version="0.2.0",
+    version="0.2.1",
     description="Run distributed Jupyter-based workflows with TaskVine",
     long_description=long_description,
     long_description_content_type="text/markdown",
